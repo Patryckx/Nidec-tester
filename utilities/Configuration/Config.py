82bbb20da3ca5616ab78d79c7bc44345fe70cf3c
Ewin <lecter933@gmail.com>
2025-01-14 17:19:08 -0600

    Implemented configuration no text warning and minor bug fixes

--- utilities/Configuration/Config.py
+++ utilities/Configuration/Config.py
@@ -21,7 +21,7 @@ class Configuration():
             #camera_port=config.get('Camera', 'port', fallback='None').replace('"', '')
 
             timer=config.get('Timer', 'cycle_time', fallback='None')
-            return gateway_port, rs485_port, rs232_port,camera_address,timer
+            return gateway_port,rs232_port, rs485_port,camera_address,timer
         except Exception as e:
             print(f"Error reading settings.ini: {e}")
             return None,None,None,None,None,None
