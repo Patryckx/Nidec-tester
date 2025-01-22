326826a2a5fc987f5445bcf6bd2ae83a2634a137
Patryckx <lecter933@gmail.com>
2025-01-21 22:59:47 -0600

    Fixed compiler bug path

--- compiler.bat
+++ compiler.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Nidec-tester\virtualenv\Scripts & activate & cd C:\Nidec-tester & pyinstaller --distpath "C:\Nidec-tester\deploy" --hidden-import=pyvisa --hidden-import=pyvisa_py --onedir --noconfirm --noconsole --icon="C:\Nidec-tester\resources\app_icon.ico" application.py --name Nidec-tester & pause & exit"
+cmd /k "cd /d C:\Nidec-tester\virtualenv\Scripts & activate & cd C:\Nidec-tester & pyinstaller --distpath "C:\Nidec-tester-release\deploy" --hidden-import=pyvisa --hidden-import=pyvisa_py --onedir --noconfirm --noconsole --icon="C:\Nidec-tester\resources\app_icon.ico" application.py --name Nidec-tester & pause & exit"
