526bdb8b2f221410cdbb37051869fc02898488f6
robotcell <robotcell@hotmail.com>
2025-02-19 11:55:58 -0600

    Vision trigger response fix

--- application.py
+++ application.py
@@ -113,9 +113,9 @@ class Test3Thread(QThread):
         raw_change_program = 'PW,'
         change_program = raw_change_program + self.led_program
         self.Camera.send_data(change_program)
-        time.sleep(1)
+        #time.sleep(1)
+        #elf.Camera.send_data('T2')
         self.Camera.send_data('T2')
-        #self.Camera.send_data('T2')
 
         # Leer la respuesta de la cámara
         results = self.Camera.read_data()
@@ -168,13 +168,17 @@ class Test4Thread(QThread):
         raw_change_program = 'PW,'
         change_program = raw_change_program + self.ocr_program
         self.Camera.send_data(change_program)
-        time.sleep(1)
         self.Camera.send_data('T2')
-        self.Camera.send_data('T2')
+        #time.sleep(1)
+        #self.Camera.send_data('T2')
+        #self.Camera.send_data('T2')
 
         # Leer la respuesta de la cámara
-        results = self.Camera.read_data()
-        resultados_herramientas = self.procesar_respuesta(results)
+        results_ocr = self.Camera.read_data()
+
+
+        print(results_ocr)
+        resultados_herramientas = self.procesar_respuesta(results_ocr)
         print(resultados_herramientas)
 
 
@@ -195,7 +199,7 @@ class Test4Thread(QThread):
         resultados_herramientas_ocr = self.procesar_respuesta(results)
         print(resultados_herramientas_ocr)'''
 
-        test_4_results = "FAIL" if "NG" in results else "PASS"
+        test_4_results = "FAIL" if "NG" in results_ocr else "PASS"
 
         
 
